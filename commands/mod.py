69a6a7ea607d85eba293a01a3ea24d843d22079f
Ben Tettmar <bentettmar@yahoo.com>
2022-09-10 19:56:59 +0100

    Added clear, ban and kick commands

--- commands/mod.py
+++ commands/mod.py
@@ -1,3 +1,5 @@
+import discord
+
 from discord.ext import commands
 from utils import config
 from utils import codeblock
@@ -21,7 +23,27 @@ class Mod(commands.Cog):
 
         await ctx.send(msg, delete_after=cfg.get("message_settings")["auto_delete_delay"])
 
-    # @commands.command(name="clear", description="Clear a number of messages.", aliases=["purge"], usage="[number]")
+    @commands.command(name="clear", description="Clear a number of messages.", aliases=["purge"], usage="[number]")
+    async def clear(self, ctx, number: int):
+        cfg = config.Config()
+        deleted = await ctx.channel.purge(limit=number + 1)
+        await ctx.send(codeblock.Codeblock(f"clear", extra_title=f"Purged {len(deleted) - 1} messages."), delete_after=cfg.get("message_settings")["auto_delete_delay"])
+
+    @commands.command(name="ban", description="Ban a member from the command server.", usage="[member]")
+    async def ban(self, ctx, member: discord.Member):
+        try:
+            await member.ban()
+            await ctx.send(codeblock.Codeblock(f"ban", extra_title=f"Banned {member.name}#{member.discriminator}"))
+        except Exception as e:
+            await ctx.send(codeblock.Codeblock(f"error", extra_title=str(e)))
+
+    @commands.command(name="kick", description="Kick a member from the command server.", usage="[member]")
+    async def kick(self, ctx, member: discord.Member):
+        try:
+            await member.kick()
+            await ctx.send(codeblock.Codeblock(f"kick", extra_title=f"Kicked {member.name}#{member.discriminator}"))
+        except Exception as e:
+            await ctx.send(codeblock.Codeblock(f"error", extra_title=str(e)))
 
 def setup(bot):
     bot.add_cog(Mod(bot))
